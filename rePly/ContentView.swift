6b4460bc7c93122fb3db6fa714af3f67abe87f74
ryota <kurokawa.ryota.1124@gmail.com>
2024-07-02 14:28:40 +0900

    initialization

---
+++ rePly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rePly
+//
+//  Created by 黒川良太 on 2024/07/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rePly/rePlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  rePlyApp.swift
+//  rePly
+//
+//  Created by 黒川良太 on 2024/07/02.
+//
+
+import SwiftUI
+
+@main
+struct rePlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
